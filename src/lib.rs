92835f890c941ec92bbb637c60881d6db451f8e4
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-08-22 20:22:21 -0700

    Alternative PAVA, still O(n)

--- src/lib.rs
+++ src/lib.rs
@@ -116,7 +116,51 @@ pub fn init_seq(x: &[f64]) -> InitSeq {
             k += 1;
         }
     }
+    // p.truncate(n - 1);
+    // nu.truncate(n - 1);
+    // for (p_j, nu_j) in p.into_iter().zip(nu.into_iter()) {
+    //     let mu = p_j / nu_j as f64;
+    //     for _ in 0..nu_j {
+    //         gamma_hat[k] = gamma_hat[k - 1] + mu;
+    //         k += 1;
+    //     }
+    // }
     let gamma_con: Vec<f64> = gamma_hat.into_iter().collect();
+
+    // let mut nu = diff(&gamma_hat);
+    // let k = nu.len();
+    // let mut w: Vec<usize> = Vec::with_capacity(k);
+    // w.resize(k, 1);
+    // let mut j = k - 1;
+    // loop {
+    //     while j > 0 && nu[j - 1] <= nu[j] {
+    //         j -= 1;
+    //     }
+    //     if j == 0 {
+    //         break;
+    //     }
+    //     let w_prime = w[j - 1] + w[j];
+    //     let w_j_m1 = w[j - 1] as f64;
+    //     let w_j = w[j] as f64;
+    //     let nu_prime = (w_j_m1 * nu[j - 1] + w_j * nu[j]) / w_prime as f64;
+    //     nu.remove(j);
+    //     w.remove(j);
+    //     nu[j - 1] = nu_prime;
+    //     w[j - 1] = w_prime;
+    //     // Adjacent violators were pooled, thus check the newly formed block
+    //     // against the (new) preceding block. However, if we pooled the
+    //     // penultimate and last blocks, then no (new) preceding block exists,
+    //     // and we must move the index left.
+    //     j = j.min(nu.len() - 1);
+    // }
+    // let mut gamma_con = gamma_hat.into_iter().collect();
+    // let mut pos: usize = 1;
+    // for (nu_i, w_i) in nu.into_iter().zip(w.into_iter()) {
+    //     for _ in 0..w_i {
+    //         gamma_con[pos] = gamma_con[pos - 1] + nu_i;
+    //         pos += 1;
+    //     }
+    // }
 
     let var_pos = 2.0 * gamma_pos.iter().sum::<f64>() - gamma_0;
     let var_dec = 2.0 * gamma_dec.iter().sum::<f64>() - gamma_0;
@@ -131,6 +175,18 @@ pub fn init_seq(x: &[f64]) -> InitSeq {
         gamma_con,
     }
 }
+// fn diff(x: &[f64]) -> Vec<f64> {
+//     let n = x.len();
+//     let mut dx: Vec<f64> = Vec::with_capacity(n - 1);
+
+//     for i in 0..n - 1 {
+//         // We know this is in bounds given that the length is n, and
+//         // (n - 2 + 1) = n - 1 is the last offset accessed.
+//         let delta = unsafe { *x.get_unchecked(i + 1) - *x.get_unchecked(i) };
+//         dx.push(delta);
+//     }
+//     dx
+// }
 
 #[cfg(test)]
 mod tests {
